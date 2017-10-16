445e4656b1a274e987c61a6871e87ff9fec38c3e
Djalma Araujo <djalma@close.io>
2017-10-16 18:55:03 +0100

    Fix distribution

--- src/main/index.js
+++ src/main/index.js
@@ -1,5 +1,6 @@
 import { app } from 'electron'
 import menubar from 'menubar'
+import path from 'path'
 import DevUtils from './utils/dev'
 import we from './services/wedeploy'
 import updateUI from './services/ui'
@@ -12,7 +13,7 @@ const url = isDevelopment
 const mb = menubar({
   index: url,
   showDockIcon: true,
-  icon: 'IconTemplate.png',
+  icon: path.join(app.getAppPath(), '/static/IconTemplate.png'),
   alwaysOnTop: true
 })
 
